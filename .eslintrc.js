b8813ad94f60dbde4c92fbab68a5dcbbea3b68a7
i-katas <feexonj@gmail.com>
2019-08-02 16:35:14 +0800

    install eslint to check code style

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "jest": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 2015,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
