bf4ee03cef4019d53bdf9b0bb7a1c4687a656692
i-katas <feexonj@gmail.com>
2019-08-02 16:26:10 +0800

    replace while loop with for loop instead

--- src/prime-factors.js
+++ src/prime-factors.js
@@ -1,13 +1,11 @@
 export default n => {
   let primes = []
-  let m = 2
 
-  while(n > 1) {
+  for(let m = 2; n > 1; m++) {
     while(n % m == 0) {
       primes.push(m)
       n/=m
     }
-    m++
   }
 
   return primes
